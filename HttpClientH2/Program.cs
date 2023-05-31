f2b4d7fa58d87dcdd38fb7284c304c7ed4e5a7c1
Yowko Tsai <yowko.tsai@skylinetw.com>
2023-05-31 14:22:00 +0800

    repo init

---
+++ HttpClientH2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+
+HttpClient myHttpClient = new HttpClient
+{
+    DefaultRequestVersion = HttpVersion.Version20,
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower
+};
+string requestUrl = "https://http2.pro/api/v1";
+try
+{
+    Console.WriteLine($"GET {requestUrl}.");
+    HttpResponseMessage response = await myHttpClient.GetAsync(requestUrl);
+    response.EnsureSuccessStatusCode();
+    Console.WriteLine($"Response HttpVersion: {response.Version}");
+    string responseBody = await response.Content.ReadAsStringAsync();
+    Console.WriteLine($"Response Body Length is: {responseBody.Length}");
+    Console.WriteLine($"------------Response Body------------");
+    Console.WriteLine(responseBody);
+    Console.WriteLine($"------------End of Response Body------------");
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"HttpRequestException : {e.Message}");
+}
+Console.WriteLine($"Press Enter to exit....");
+Console.ReadLine();
